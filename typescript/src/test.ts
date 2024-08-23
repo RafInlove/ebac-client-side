084dffe92d766e233c3f86d31d39e946ded5dc17
R <raphael18pro@gmail.com>
2024-08-23 10:49:23 -0300

    finalizando exercício

---
+++ typescript/src/test.ts
@@ -0,0 +1,18 @@
+let paixao = "Vitória";
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+
+const resultadoMultiplicacao = multiplicar(5, 3);
+const resultadoSaudacao = saudacao("Vitória");
+
+console.log(resultadoMultiplicacao);
+console.log(resultadoSaudacao);   
+
