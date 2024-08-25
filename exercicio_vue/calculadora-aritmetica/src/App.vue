6d9881ef4b01f1a2be6ece7f1c27780fa5fc9a35
R <raphael18pro@gmail.com>
2024-08-25 20:41:58 -0300

    finalizando exercício

---
+++ exercicio_vue/calculadora-aritmetica/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CalculadoraAritmetica/>
+</template>
+
+<script>
+import CalculadoraAritmetica from './components/CalculadoraAritmetica.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalculadoraAritmetica
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ exercicio_vue/calculadora-aritmetica/src/components/CalculadoraAritmetica.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="calculator">
+    <h1>Calculadora Aritmética</h1>
+    <input type="number" v-model.number="num1" placeholder="Número 1" />
+    <input type="number" v-model.number="num2" placeholder="Número 2" />
+
+    <select v-model="operation">
+      <option value="+">Adição</option>
+      <option value="-">Subtração</option>
+      <option value="*">Multiplicação</option>
+      <option value="/">Divisão</option>
+    </select>
+
+    <h2>Resultado: {{ calculateResult }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operation: '+',
+    };
+  },
+  computed: {
+    calculateResult() {
+      switch (this.operation) {
+        case '+':
+          return this.num1 + this.num2;
+        case '-':
+          return this.num1 - this.num2;
+        case '*':
+          return this.num1 * this.num2;
+        case '/':
+          return this.num2 !== 0 ? this.num1 / this.num2 : 'Indefinido';
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.calculator {
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+}
+
+input, select {
+  margin: 10px 0;
+  padding: 8px;
+  width: 100%;
+}
+</style>
