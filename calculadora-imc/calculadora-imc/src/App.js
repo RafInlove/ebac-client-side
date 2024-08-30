de39c81aef8f35198ff12342e22bd40a74864c9c
R <raphael18pro@gmail.com>
2024-08-30 19:01:13 -0300

    finalizando exercício

---
+++ calculadora-imc/calculadora-imc/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IMCForm from './IMCform';
+
+function App() {
+  return (
+    <div className="App">
+      <IMCForm />
+    </div>
+  );
+}
+
+export default App;
