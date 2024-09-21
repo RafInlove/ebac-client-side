82d105c92d309782709dc1217721ef51dab99074
R <raphael18pro@gmail.com>
2024-09-21 12:04:34 -0300

    finalizando exercício

---
+++ jest/saudacao.test.js
@@ -0,0 +1,9 @@
+const saudacao = require('./saudacao')
+
+describe('Testes para saudacao', () => {
+    test('Deverá retornar Olá, Maria', () => {
+        const olaMaria = saudacao.dizOla('Maria')
+        expect(olaMaria).toBe('Olá, Maria')
+        expect(olaMaria).toContain(',')
+    })
+})
---
+++ primeiro_projeto_vue/vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  const titulo = "Salamaleico";
+</script>
+
+<template>
+<h1>{{ titulo }}</h1>
+</template>
+
+<style scoped>
+
+</style>
